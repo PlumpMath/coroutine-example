fca0aca2c47b02d7330a038d5f9c91f36ddd45ee
Bede Kelly <bedekelly97@gmail.com>
2016-03-25 13:58:25 +0000

    Re-order functions for clarity

--- restaurant.py
+++ restaurant.py
@@ -1,17 +1,17 @@
 from kitchen import good_kitchen as kitchen
 
 
+def get_orders():
+    """Continuously get food orders from the diners."""
+    while True:
+        yield input()
+
+
 def run_restaurant(kitchen=None, serve=None):
     """Prepare and serve each order as it comes."""
     for order in get_orders():
         food = kitchen.send(order)
         serve(food)
-
-
-def get_orders():
-    """Continuously get food orders from the diners."""
-    while True:
-        yield input()
 
 
 def fancy_serve(food):
